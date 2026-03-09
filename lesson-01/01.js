1645100dc67bb1d8cde2533ffd97f245539a1c4d
viktorKorneev <korneevviktor90@yandex.ru>
2026-03-09 08:12:44 +0300

    first-commit

---
+++ lesson-01/01.js
@@ -0,0 +1,12 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+let number = 0;
+let sum = 0;
+
+while (number < 20) {
+  number++;
+  sum += number;
+}
+console.log(sum);
