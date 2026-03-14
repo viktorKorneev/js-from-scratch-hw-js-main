38fa16d582865c04d446e800694912f93361d042
viktorKorneev <korneevviktor90@yandex.ru>
2026-03-14 17:55:00 +0300

    task-1

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -10,9 +10,15 @@
 // тестовые данные (значения можно менять)
 const isAdmin = false
 const isVerifiedUser = true
-const hasSpecialPermission = true
-const hasTemporaryPass = false
+const hasSpecialPermission = false
+const hasTemporaryPass = true
 
 let isAccess
 
 // your code
+if((isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass)) {
+    isAccess = true
+} else {
+    isAccess = false
+}
+
