8ad75566492c6584c63d94648a694a54b9cd1504
viktorKorneev <korneevviktor90@yandex.ru>
2026-03-14 18:34:14 +0300

    task-3

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,20 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
-let grade
+const score = 30; // тестовое значение, можно изменять
+let grade;
 // your code
+
+if (score <= 100 && score >= 90) {
+  grade = "A";
+} else if (score <= 89 && score >= 80) {
+  grade = "B";
+} else if (score <= 79 && score >= 70) {
+  grade = "C";
+} else if (score <= 69 && score >= 50) {
+  grade = "D";
+} else {
+  grade = "F";
+}
+
+
