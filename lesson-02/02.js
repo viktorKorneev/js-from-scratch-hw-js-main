d836ee5920a67cf01bd5c3c2bf271bf5403fc5ed
viktorKorneev <korneevviktor90@yandex.ru>
2026-03-14 18:22:33 +0300

    task-2

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,11 +7,14 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = Math.floor(Math.random() * 6 + 1); // your code
+let dice2 = Math.floor(Math.random() * 6 + 1); // your code
+let isWinningDouble = false; // your code
 
-/*
+if ((dice1 === dice2 && dice1 > 3 && dice2 > 3)) {
+  isWinningDouble = true;
+}
+
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
 if (isWinningDouble) {
@@ -19,4 +22,5 @@ if (isWinningDouble) {
 } else {
   console.log('Не выигрышный дубль.')
 }
-*/
+
+
