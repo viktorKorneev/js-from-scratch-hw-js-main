5ab180e93aa78e309e3cded9be1ebf4d5780065b
viktorKorneev <korneevviktor90@yandex.ru>
2026-03-20 21:02:17 +0300

    task-3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,12 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(num_1, num_2, num_3) {
+  if (num_1 > num_2 && num_1 > num_3) {
+    return num_1;
+  } else if (num_2 > num_1 && num_2 > num_3) {
+    return num_2;
+  } else {
+    return num_3;
+  }
+}
+console.log(findLargest(5, 7, 9));
