8cd3ec5425daa49ba89545a70ec2f5fa60475e38
viktorKorneev <korneevviktor90@yandex.ru>
2026-03-31 23:09:11 +0300

    task-4

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,4 +12,20 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(str) {
+  let arr = [];
+  let newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    arr.push(str[i] + str[i]);
+  }
+  arr = `${arr}`;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== ",") {
+      newStr += arr[i];
+    }
+  }
+  return newStr;
+}
+
+console.log(doubleEachCharacter("hello"));
