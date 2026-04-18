7e47d9c8a11ef66acac1a3f0ac6fe0d5a086da1b
viktorKorneev <korneevviktor90@yandex.ru>
2026-04-18 23:48:28 +0300

    task-1

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -23,4 +23,20 @@ const oddNumbers = filter(numbers, (element, index) => {
 console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
 
-const filter = () => {}
+const numbers = [1, 2, 3, 4, 5]
+
+const filter = (array, callback) => {
+  let newArray = [];
+  for(let i = 0; i < array.length; i++) {
+    if(oddNumbers(array[i], i)) {
+      newArray.push(array[i])
+    }
+  }
+  return newArray;
+}
+
+const oddNumbers = (element, index) => {
+  return element % 2 !== 0
+};
+
+console.log(filter(numbers, oddNumbers()));
