ca419ed5bf74870fc327981ce040a43eb5803db4
viktorKorneev <korneevviktor90@yandex.ru>
2026-05-22 00:08:56 +0300

    task-2

--- lesson-08/02.js
+++ lesson-08/02.js
@@ -4,11 +4,16 @@
  */
 
 function isNumeric(str) {
-  // your code
+  str = Number(str);
+  if (str * 1) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-// console.log(isNumeric("123")) // Ожидаемый результат: true
-// console.log(isNumeric("12.3")) // Ожидаемый результат: true
-// console.log(isNumeric("123abc")) // Ожидаемый результат: false
-// console.log(isNumeric("abc")) // Ожидаемый результат: false
-// console.log(isNumeric(" ")) // Ожидаемый результат: false
+console.log(isNumeric("123")); // Ожидаемый результат: true
+console.log(isNumeric("12.3")); // Ожидаемый результат: true
+console.log(isNumeric("123abc")); // Ожидаемый результат: false
+console.log(isNumeric("abc")); // Ожидаемый результат: false
+console.log(isNumeric(" ")); // Ожидаемый результат: false
