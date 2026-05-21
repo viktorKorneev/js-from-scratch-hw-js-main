d10d4747e37835c2ef9133fd6375293d3d78ba3d
viktorKorneev <korneevviktor90@yandex.ru>
2026-05-22 00:20:01 +0300

    task-3

--- lesson-08/03.js
+++ lesson-08/03.js
@@ -10,5 +10,6 @@
 */
 
 function truncate(str, maxLength) {
-  // your code
+  return str.length > maxLength ? str.slice(0, maxLength) + "..." : str;
 }
+console.log(truncate("Вот, что мне действительно нравится в этом", 20));
