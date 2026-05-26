6f6f60f160ca1914d5e8b2fd9897e3ac7df1e529
viktorKorneev <korneevviktor90@yandex.ru>
2026-05-26 20:08:17 +0300

    task-1

--- lesson-10/01.js
+++ lesson-10/01.js
@@ -24,37 +24,50 @@
 const model = {
   movies: [],
   addMovie(title, description) {
-    const id = Math.random()
-    const newMovie = { id, title, description }
-    this.movies.push(newMovie)
-    view.renderMovies(this.movies)
+    const id = Math.random();
+    const newMovie = { id, title, description };
+    this.movies.push(newMovie);
+    view.renderMovies(this.movies);
   },
   // your code
-}
+  deleteMovie(movieId) {
+    this.movies = this.movies.filter((movie) => movie.id !== movieId);
+    view.renderMovies(this.movies);
+  },
+};
+console.log(model.movies);
 
 const view = {
   init() {
-    this.renderMovies(model.movies)
+    this.renderMovies(model.movies);
 
-    const form = document.querySelector('.form')
-    const inputTitle = document.querySelector('.input-title')
-    const inputDescription = document.querySelector('.input-description')
+    const form = document.querySelector(".form");
+    const inputTitle = document.querySelector(".input-title");
+    const inputDescription = document.querySelector(".input-description");
 
-    form.addEventListener('submit', function (event) {
-      event.preventDefault()
-      const title = inputTitle.value
-      const description = inputDescription.value
-      controller.addMovie(title, description)
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+      const title = inputTitle.value;
+      const description = inputDescription.value;
+      controller.addMovie(title, description);
 
-      inputTitle.value = ''
-      inputDescription.value = ''
-    })
+      inputTitle.value = "";
+      inputDescription.value = "";
+    });
 
     // your code
+    const list = document.querySelector(".list");
+    list.addEventListener("click", function (event) {
+      if (event.target.classList.contains("delete-button")) {
+        const li = event.target.closest("li");
+        const movieId = Number(li.id);
+        controller.deleteMovie(movieId);
+      }
+    });
   },
   renderMovies(movies) {
-    const list = document.querySelector('.list')
-    let moviesHTML = ''
+    const list = document.querySelector(".list");
+    let moviesHTML = "";
 
     for (const movie of movies) {
       moviesHTML += `
@@ -63,38 +76,42 @@ const view = {
           <p class="movie-description">${movie.description}</p>
           <button class="delete-button" type="button">Удалить 🗑</button>
         </li>
-      `
+      `;
     }
 
-    list.innerHTML = moviesHTML
+    list.innerHTML = moviesHTML;
   },
   displayMessage(message, isError = false) {
-    const messageBox = document.querySelector('.message-box')
-    messageBox.textContent = message
+    const messageBox = document.querySelector(".message-box");
+    messageBox.textContent = message;
     if (isError) {
-      messageBox.classList.remove('success')
-      messageBox.classList.add('error')
+      messageBox.classList.remove("success");
+      messageBox.classList.add("error");
     } else {
-      messageBox.classList.remove('error')
-      messageBox.classList.add('success')
+      messageBox.classList.remove("error");
+      messageBox.classList.add("success");
     }
   },
-}
+};
 
 const controller = {
   addMovie(title, description) {
-    if (title.trim() !== '' && description.trim() !== '') {
-      model.addMovie(title, description)
-      view.displayMessage('Фильм добавлен успешно!')
+    if (title.trim() !== "" && description.trim() !== "") {
+      model.addMovie(title, description);
+      view.displayMessage("Фильм добавлен успешно!");
     } else {
-      view.displayMessage('Заполните все поля!', true)
+      view.displayMessage("Заполните все поля!", true);
     }
   },
   // your code
-}
+  deleteMovie(movieId) {
+    model.deleteMovie(movieId);
+    view.displayMessage("Фильм успешно удалён!");
+  },
+};
 
 function init() {
-  view.init()
+  view.init();
 }
 
-document.addEventListener('DOMContentLoaded', init)
+document.addEventListener("DOMContentLoaded", init);
